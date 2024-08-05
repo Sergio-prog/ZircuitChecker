14aa861cca133f7ae6006efca4fe4e277c096633
Sergey Nesterov <sergioreklamplay@gmail.com>
2024-08-05 22:23:55 +0300

    Initial commit

--- main.py
+++ main.py
@@ -27,17 +27,20 @@ def main():
 
     print("========================================================")
 
+    total_amount = 0
     for wallet in wallets:
         eligible_amount = round(fetch_drop_amount(wallet, session), 3)
         if eligible_amount <= 0:
             print(f"You are not eligible (0 $ZRC) in {wallet!r} account")
         else:
             print(f"You eligible for {eligible_amount} in {wallet!r} account")
+            total_amount += eligible_amount
         print("========================================================\n")
 
         sleep(1)
 
     print("All wallets has been verified")
+    print(f"Total amount: {total_amount}")
 
 
 if __name__ == '__main__':
